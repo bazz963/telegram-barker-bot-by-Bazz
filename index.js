7526aa0702d6516e16299809f0b0e231a0adc63e
bazz963 <bazzattang9917@gmail.com>
2026-07-18 00:36:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 
 // Make sure your real token from @BotFather is inside these single quotes
 const BOT_TOKEN = '8620955030:AAWzxRgUtAFwUvXlhfIfwkyDZw8452M4uc'; 
 
 const bot = new Telegraf(BOT_TOKEN);
 
+// Helper function to create stylish buttons under messages
+function getStylishButtons() {
+    return Markup.inlineKeyboard([
+        [
+            // Replace with your real Telegram channel link!
+            Markup.button.url('📢 Join Our Channel', 'https://t.me/bazzstore963'),
+            // This safely links straight to your developer profile!
+            Markup.button.url('💻 Developer', 'https://t.me/BazzHacker963')
+        ]
+    ]);
+}
+
 // 1. /start command
 bot.start((ctx) => {
+    // Put your uploaded image link here when you have it!
     const photoUrl = 'https://images.unsplash.com/photo-1543466835-00a7907e9de1?w=500'; 
+    
     ctx.replyWithPhoto(photoUrl, {
-        caption: `🐾 Welcome to the Spam Bot! 🐾\n\nUse the command like this:\n/spam 100 hi guys\n\nType /dev to see who made me!`
+        caption: `🐾 Welcome to the Spam Bot! 🐾\n\nUse the command like this:\n/spam 100 hi guys`,
+        ...getStylishButtons()
     });
 });
 
-// 2. /spam [number] [message] command (Sends multiple separate messages up to 100!)
+// 2. /spam [number] [message] command
 bot.command('spam', async (ctx) => {
     const messageText = ctx.message.text.trim();
     const args = messageText.split(' '); 
@@ -21,28 +36,25 @@ bot.command('spam', async (ctx) => {
     const numberOfSpams = parseInt(countInput);
 
     if (!countInput || isNaN(numberOfSpams)) {
-        return ctx.reply("Please give me a number and a message! Example: /spam 100 hi guys");
+        return ctx.reply("Please give me a number and a message! Example: /spam 100 hi guys", getStylishButtons());
     }
 
-    // New limit set exactly to 100 as requested!
     if (numberOfSpams > 100) {
-        return ctx.reply("Please keep the limit to 100 separate messages or less!");
+        return ctx.reply("Please keep the limit to 100 separate messages or less!", getStylishButtons());
     }
 
-    // Grabs your custom text message
     const commandLength = args[0].length + args[1].length + 2; 
     const customMessage = messageText.substring(commandLength).trim();
     const textToRepeat = customMessage || "Spam! 🤖";
 
-    // Loops and sends individual messages up to 100 times
+    // Loops and sends individual messages
     for (let i = 0; i < numberOfSpams; i++) {
-        await ctx.reply(textToRepeat);
+        if (i === numberOfSpams - 1) {
+            await ctx.reply(textToRepeat, getStylishButtons());
+        } else {
+            await ctx.reply(textToRepeat);
+        }
     }
-});
-
-// 3. /dev command
-bot.command('dev', (ctx) => {
-    ctx.reply("💻 Spam Bot Developer 💻\n\nThis bot was proudly created by a legendary developer!");
 });
 
 // Start the bot securely
@@ -51,7 +63,7 @@ async function startBot() {
         console.log('Connecting to Telegram...');
         await bot.launch();
         console.log('--- SUCCESS ---');
-        console.log('Spam Bot is officially ONLINE and ready!');
+        console.log('Spam Bot is officially ONLINE!');
     } catch (error) {
         console.error('Launch Error:', error);
     }
@@ -64,6 +76,7 @@ setInterval(() => {}, 100000);
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
 
 
 
